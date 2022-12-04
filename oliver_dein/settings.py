6b7fb2e8fede283269f90fe9d8ffe30614329419
Shahidul Islam <shahid.okay@gmail.com>
2022-12-04 23:15:35 +0000

    Added home app and template

--- oliver_dein/settings.py
+++ oliver_dein/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -61,12 +62,15 @@ CSRF_TRUSTED_ORIGINS = ['https://*.mydomain.com', 'https://8000-shahid129-oliver
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  #required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
