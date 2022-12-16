2bd8e749def2d396cb18b77872fb6a4b793346b7
Shahidul Islam <shahid.okay@gmail.com>
2022-12-16 21:03:52 +0000

    Added style to header

--- products/models.py
+++ products/models.py
@@ -11,13 +11,9 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=100)
-    friendly_name = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
 
 
 class Product(models.Model):
