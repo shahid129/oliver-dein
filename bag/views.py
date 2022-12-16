e389e9a136771beb2874a6ca440079d87edf1a5f
Shahidul Islam <shahid.okay@gmail.com>
2022-12-16 21:26:08 +0000

    Added  bag app and templates

---
+++ bag/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def bag(request):
+    """
+    A view to return the contents in the bag page
+    """
+    return render(request, 'bag/bag.html')
