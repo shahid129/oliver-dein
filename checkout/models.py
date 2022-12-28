51d2f1f7cdebee5d015936c7236112f64d3d83fb
Shahidul Islam <shahid.okay@gmail.com>
2022-12-28 15:08:33 +0000

    Added checkout success logic and page

--- checkout/models.py
+++ checkout/models.py
@@ -17,28 +17,23 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(
-        max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, default=0)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
         """
         Generate random and unique order number using UUID
         """
-        return uuid.uuid4().hex.upper
+        return uuid.uuid4().hex.upper()
 
     def update_total(self):
         """
         Update grand total each time a item is added
         """
-        self.order_total = self.lineitems.aggregate(sum('lineitem_total'))[
-            'lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * \
-                settings.STANDARD_DELOVERY_PERCENTAGE / 100
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
@@ -51,23 +46,18 @@ class Order(models.Model):
         """
         if not self.order_number:
             self.order_number = self._generate_order_number()
-            super().save(*args)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.order_number
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False,
-                              on_delete=models.CASCADE,
-                              related_name='lineitems')
-    product = models.ForeignKey(
-        Product, null=False, blank=False, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(
-        max_digits=6, decimal_places=2, null=False,
-        blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """"
@@ -78,7 +68,7 @@ class OrderLineItem(models.Model):
             self.lineitem_total = self.product.discount_price * self.quantity
         else:
             self.lineitem_total = self.product.price * self.quantity
-        super().save(*args)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'SKU { self.product.sku } on order { self.order.order_number }'
