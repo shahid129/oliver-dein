9b5593eb7273c9e637f82b8872fb26cbc7fb9a73
Shahidul Islam <shahid.okay@gmail.com>
2023-01-01 22:45:19 +0000

    Added add-product funtionalities

--- profiles/views.py
+++ profiles/views.py
@@ -19,8 +19,11 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Failed to Update. Please ensure the form is valid.')
 
-    form = UserProfileForm(instance=profile)
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
