15f3c941867436c33b8f9a0b767531a4d63ed306
Shahidul Islam <shahid.okay@gmail.com>
2023-01-05 23:06:31 +0000

    Added test-url faqs app

--- faqs/test_views.py
+++ faqs/test_views.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
 from .models import Faqs
 
 
@@ -6,7 +8,44 @@ class TestViews(TestCase):
     """
     Test for the views
     """
-    def test_faqs(self):
+    # ----------------- Test all the urls
+    def setUp(self):
+        self.superuser = User.objects.create_superuser(
+            'admin', 'admin@email.com', 'adminpassword'
+        )
+        
+        self.user = User.objects.create_user(
+            'shahid', 'shahid@email.com', 'shahidpassword'
+        )
+
+        self.faq = Faqs.objects.create(
+            category='OR',
+            questions='sample question',
+            answers='sample answers'
+        )
+
+    def test_faqs_page(self):
+        """
+        Test faqs page
+        """
         response = self.client.get('/faqs/')
         self.assertEqual(response.status_code, 200)
-        self. assertTemplateUsed(response, 'faqs/faqs.html')
+        self.assertTemplateUsed(response, 'faqs/faqs.html')
+
+    def test_add_faqs_page(self):
+        """
+        Test if the add_faqs page loads
+        """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get('/faqs/add_faqs/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'faqs/add_faqs.html')
+
+    def test_edit_faqs_page(self):
+        """
+        Test if the edit page loads
+        """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get(f'/faqs/edit_faqs/{self.faq.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'faqs/edit_faqs.html')
--- oliver_dein/settings.py
+++ oliver_dein/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'oliver_dein.urls'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-CSRF_TRUSTED_ORIGINS = ['https://*.mydomain.com', 'https://*.8000-shahid129-oliverdein-u73z2t1mzzl.ws-eu80.gitpod.io/']
+CSRF_TRUSTED_ORIGINS = ['https://*.mydomain.com', 'https://*.8000-shahid129-oliverdein-u73z2t1mzzl.ws-eu81.gitpod.io/']
 
 TEMPLATES = [
     {
