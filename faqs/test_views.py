690549f1e98f8f9084d390bc8759f12fada66585
Shahidul Islam <shahid.okay@gmail.com>
2023-01-06 19:56:13 +0000

    Added test to faqs-app

--- faqs/test_views.py
+++ faqs/test_views.py
@@ -49,3 +49,107 @@ class TestViews(TestCase):
         response = self.client.get(f'/faqs/edit_faqs/{self.faq.id}/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'faqs/edit_faqs.html')
+
+    def test_delete_faqs_page(self):
+        """
+        Test to check if delete page redirects
+        """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get(f'/faqs/delete_faqs/{self.faq.id}/')
+        self.assertEqual(response.status_code, 302)  
+        # self.assertRedirects(response, '/faqs/')
+
+    # ----------------- End of url test
+
+    def test_add_faqs(self):
+        """
+        Test if the view creates a new faqs
+        """
+        self.client.login(username='admin', password='adminpassword')
+        # check if the form is valid
+        response = self.client.post(
+            '/faqs/add_faqs/', {
+                'category': 'OT',
+                'questions': 'Sample Question',
+                'answers': 'Sample Answers'
+            }, follow=True  # the client will follow the redirect
+        )
+        self.assertEqual(response.status_code, 200)
+
+        message = list(response.context.get('messages'))[0]
+        self.assertEqual(message.tags, "success")
+        self.assertTrue("FAQs added successfully." in message.message)
+
+    def test_edit_faqs(self):
+        """
+        Test if the edit view lets you edit the faqs
+        """
+        self.client.login(username='admin', password='adminpassword')
+        # check if the form is valid
+        response = self.client.post(
+            f'/faqs/edit_faqs/{self.faq.id}/', {
+                'category': 'OT',
+                'questions': 'Sample Question',
+                'answers': 'Sample Answers'
+            }, follow=True  # the client will follow the redirect
+        )
+        self.assertEqual(response.status_code, 200)
+
+        message = list(response.context.get('messages'))[0]
+        self.assertEqual(message.tags, "success")
+        self.assertTrue("FAQs updated successfully." in message.message)
+
+    def test_delete_faqs(self):
+        """
+        Test if the edit view lets you edit the faqs
+        """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.post(
+            f'/faqs/delete_faqs/{self.faq.id}/'
+        )
+        self.assertEqual(response.status_code, 302)
+
+    # Superuser functionalites
+    def test_only_superuser_can_add(self):
+        """
+        Test if the edit view lets you edit the faqs
+        """
+        self.client.login(username='shahid', password='shahidpassword')
+        response = self.client.post(
+            f'/faqs/add_faqs/', follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+
+        message = list(response.context.get('messages'))[0]
+        self.assertEqual(message.tags, "error")
+        self.assertTrue('Sorry! You are not authorised to do that' in message.message)
+
+
+    def test_only_superuser_can_edit(self):
+        """
+        Test if the edit view lets you edit the faqs
+        """
+        self.client.login(username='shahid', password='shahidpassword')
+        response = self.client.post(
+            f'/faqs/edit_faqs/{self.faq.id}/', follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+
+        message = list(response.context.get('messages'))[0]
+        self.assertEqual(message.tags, "error")
+        self.assertTrue('Sorry! You are not authorised to do that' in message.message)
+
+    def test_only_superuser_can_delete(self):
+        """
+        Test if the edit view lets you edit the faqs
+        """
+        self.client.login(username='shahid', password='shahidpassword')
+        response = self.client.post(
+            f'/faqs/delete_faqs/{self.faq.id}/', follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+
+        message = list(response.context.get('messages'))[0]
+        self.assertEqual(message.tags, "error")
+        self.assertTrue('Sorry! You are not authorised to do that' in message.message)
+
