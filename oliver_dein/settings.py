3d38f15d8d2e09aa61e12b98f8b789b6cef35452
Shahidul Islam <shahid.okay@gmail.com>
2023-01-12 12:05:32 +0000

    Deployment commit

--- oliver_dein/settings.py
+++ oliver_dein/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPEMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['oliver-dein.herokuapp.com', 'localhost']
 
