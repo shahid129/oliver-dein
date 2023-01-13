767d53689ad07cd009c6b51d8eafd824b4ab08e2
Shahidul Islam <shahid.okay@gmail.com>
2023-01-13 13:56:08 +0000

    Added error 404 template

--- oliver_dein/urls.py
+++ oliver_dein/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +31,5 @@ urlpatterns = [
     path('faqs/', include('faqs.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'oliver_dein.views.handler404'
