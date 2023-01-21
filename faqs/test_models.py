57995b64b7c98fa63d09801c5994aef5939b06bc
Shahidul Islam <shahid.okay@gmail.com>
2023-01-21 14:51:33 +0000

    Added test models to faqs-app

---
+++ faqs/test_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from .models import Faqs
+
+
+class TestModels(TestCase):
+    """
+    Test for models
+    """
+    def test_if_it_returns_a_question(self):
+        """
+        Test to see if it returns a string
+        """
+        faqs = Faqs.objects.create(
+            category='OR',
+            questions='Test Question',
+            answers='Test Answer'
+        )
+        self.assertEqual(str(faqs), 'Test Question')
