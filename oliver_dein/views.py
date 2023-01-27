3ae3246373c8ca922c93153323c1940039260acb
Shahidul Islam <shahid.okay@gmail.com>
2023-01-27 20:57:46 +0000

    deployment commit

--- oliver_dein/views.py
+++ oliver_dein/views.py
@@ -6,6 +6,6 @@ def handler404(request, exception):
     return render(request, "errors/404.html", status=404)
 
 
-def handler500(request,):
+def handler500(request, exception=None):
     """ Error Handler 500 Internal Server error"""
     return render(request, "errors/500.html", status=500)
